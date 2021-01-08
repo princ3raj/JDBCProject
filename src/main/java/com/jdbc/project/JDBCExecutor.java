669ce4fefe19a13731166f210e61774a883021be
princ3raj <princ3raj1999@gmail.com>
2021-01-08 11:06:01 +0530

    Completed CRUD operations

---
+++ src/main/java/com/jdbc/project/JDBCExecutor.java
@@ -0,0 +1,35 @@
+package com.jdbc.project;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExecutor {
+
+
+    public static void main(String... args){
+
+            DatabaseConnectionManager dcm= new DatabaseConnectionManager("localhost"
+            ,"hplussport","postgres","");
+
+            try {
+                Connection connection= dcm.getConnection();
+                CustomerDAO customerDAO=new CustomerDAO(connection);
+                //update Data from Database
+                Customer customer=customerDAO.findById(10000);
+                System.out.println(customer.getFirstName()+" "+customer.getLastName()+" "+customer.getEmail());
+                customer.setEmail("update it yet");
+                customer=customerDAO.update(customer);
+                System.out.println(customer.getFirstName()+" "+customer.getLastName()+" "+customer.getEmail());
+
+
+
+
+            }catch (SQLException e){
+
+                e.printStackTrace();
+
+            }
+    }
+}
---
+++ src/main/java/com/jdbc/project/util/DataTransferObject.java
@@ -0,0 +1,6 @@
+package com.jdbc.project.util;
+
+public interface DataTransferObject {
+
+    long getId();
+}
